f515411008b8afc908015399b2f40820db262164
Sergey Minin <sm.minins@gmail.com>
2025-02-28 17:12:08 +0300

    реализовано дз homework_16_2

--- tests/test_base_product.py
+++ tests/test_base_product.py
@@ -3,7 +3,11 @@ from src.base_product import BaseProduct
 
 
 def test_base_product_new_product():
+
+    # Заменяем реальный метод на заглушку (mock), чтобы проверить поведение метода без выполнения
+    # реальной логики. Используем patch.object для замены метода new_product базового класса
     with patch.object(BaseProduct, 'new_product') as mock_new_product:
+
         # Вызываем метод new_product класса BaseProduct
         result = BaseProduct.new_product("arg1", arg2="value")
 
@@ -12,3 +16,9 @@ def test_base_product_new_product():
 
         # Проверка результата вызова
         assert result is not None
+
+
+def test_abstract_method():
+    """Проверка, что попытка вызвать абстрактный метод из базового класса возвращает None"""
+
+    assert BaseProduct.new_product() is None
